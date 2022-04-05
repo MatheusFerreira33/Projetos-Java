9395a22e850bebdf091c2777da3e00f5431f6a55
MatheusFerreira33 <matheus.fsilva33@gmail.com>
2022-04-05 12:07:11 -0300

    operadores_e_condicionais

---
+++ src/AluraJava_3/IncrimentosDecrementos.java
@@ -0,0 +1,13 @@
+package AluraJava_3;
+public class IncrimentosDecrementos {
+
+    
+    public static void main(String[] args) {
+         
+        int i = 5;
+        i--;
+        System.out.println(++i);
+
+        i += 2;
+    }
+}
