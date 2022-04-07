b501714b007559105c5474940cad3d232f709b1d
MatheusFerreira33 <matheus.fsilva33@gmail.com>
2022-04-07 16:44:19 -0300

    Java Programa

---
+++ src/programas/estoqueProdutos/produtos.java
@@ -0,0 +1,67 @@
+package programas.estoqueProdutos;
+
+class jogosPS4 {
+
+    double gtaV() {
+
+        double preco = 80.00;
+
+        if( preco == 80.00 ){
+            System.out.println(preco);
+        }
+
+        double quaEsto = 5;
+
+        //double view[] = new double[] { preco, quaEsto };
+
+        return quaEsto;
+       
+
+    }
+
+    double fifa21(){
+
+        double preco= 120;
+
+        if( preco == 120.00 ){
+            System.out.println(preco);
+        }
+
+        double quaEsto = 15;
+
+        return quaEsto;
+
+    }
+
+
+    double warzone(){
+
+        double preco= 200;
+
+        if( preco == 200.00 ){
+            System.out.println(preco);
+        }
+
+        double quaEsto = 8;
+
+        return quaEsto;
+
+    }
+
+
+
+
+
+
+
+
+
+} 
+
+public class produtos {
+
+   
+
+    
+
+}
